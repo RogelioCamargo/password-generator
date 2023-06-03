dc6a088a53636cd5db4bf372c52ad395c1adc7b1
Rogelio Camargo <rogelio.camargo2016@gmail.com>
2023-06-03 14:06:20 -0700

    Write form submission logic

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Slider for character length
-var slider = document.getElementById("characterLengthInput");
+var slider = document.getElementById("characterLength");
 var characterLengthContainer = document.querySelector(
   ".currentCharacterLength"
 );
@@ -12,13 +12,65 @@ slider.addEventListener("input", function updateCharacterLength() {
   slider.style.background = progressColor;
 });
 
+
+
 // Form submission
-var form = document.querySelector(".pw-form");
 form.addEventListener("submit", function onSubmit(event) {
   event.preventDefault();
 
+  var options = {
+    characterLength: 0,
+    includeLowercase: false,
+    includeUppercase: false,
+    includeNumbers: false,
+    includeSymbols: false,
+  };
   var formData = new FormData(form);
-  for (const [key, value] of formData) {
-    console.log(key, value);
+  for (const [name, value] of formData) {
+    if (value == "on") {
+      options[name] = true;
+    } else if (name == "characterLength") {
+      options[name] = parseInt(value);
+    }
   }
+
+  const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const symbols = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+  var characterPool = [];
+  var password = "";
+
+  if (options.includeLowercase) {
+    characterPool.push(lowercaseLetters);
+  }
+  if (options.includeUppercase) {
+    characterPool.push(uppercaseLetters);
+  }
+  if (options.includeNumbers) {
+    characterPool.push(numbers);
+  }
+  if (options.includeSymbols) {
+    characterPool.push(symbols);
+  }
+
+  if (!characterPool.length) {
+    return;
+  }
+
+  for (let i = 0; i < options.characterLength; i++) {
+    const randomIndex = Math.floor(Math.random() * characterPool.length);
+    const characterSet = characterPool[randomIndex];
+
+    password += getRandomCharacter(characterSet);
+  }
+
+  var passwordGenereatedInput = document.querySelector(".pw-generated__result");
+  passwordGenereatedInput.value = password;
 });
+
+function getRandomCharacter(characterSet) {
+  const randomIndex = Math.floor(Math.random() * characterSet.length);
+  return characterSet[randomIndex];
+}
