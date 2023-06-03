ba5a699e6bd46c8649ea2e4cacfe1475854ea206
Rogelio Camargo <rogelio.camargo2016@gmail.com>
2023-06-03 12:18:07 -0700

    Fix markup and update styling

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-const slider = document.getElementById("characterLength");
-const currentCharacterLength = document.querySelector(".currentCharacterLength");
+// Slider for character length
+var slider = document.getElementById("characterLengthInput");
+var characterLengthContainer = document.querySelector(
+  ".currentCharacterLength"
+);
 
-slider.addEventListener("input", function() {
-	console.log(slider.value - slider.min, slider.max - slider.min);
-  const percent = (slider.value - slider.min) / (slider.max - slider.min) * 100;
+slider.addEventListener("input", function updateCharacterLength() {
+  const percent =
+    ((slider.value - slider.min) / (slider.max - slider.min)) * 100;
   const progressColor = `linear-gradient(to right, var(--neon-green) 0%, var(--neon-green) ${percent}%, var(--black) ${percent}%, var(--black) 100%)`;
-	currentCharacterLength.textContent = slider.value;
+  characterLengthContainer.textContent = slider.value;
   slider.style.background = progressColor;
+});
+
+// Form submission
+var form = document.querySelector(".pw-form");
+form.addEventListener("submit", function onSubmit(event) {
+  event.preventDefault();
+
+  var formData = new FormData(form);
+  for (const [key, value] of formData) {
+    console.log(key, value);
+  }
 });
